0692afd61a45b66de108a74ea71c78333d7448aa
okuda <okuda@pass-system.co.jp>
2023-01-06 15:42:12 +0900

    ルートリンク完了

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -3,23 +3,34 @@ import { NavLink } from 'react-router-dom'
 
 export default class Navbar extends Component {
     render() {
+
+        let activeStyle = {
+            'color': 'tomato',
+            'textDecoration': 'none'
+        }
+
+        let activeClass = "active-link";
+
         return (
             <ul>
                 <li>
                     <NavLink
-                        style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        className={({ isActive }) => (isActive ? activeClass : undefined)}
                         to="/"
                     >Home</NavLink>
                 </li>
                 <li>
                     <NavLink
-                        style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        className={({ isActive }) => (isActive ? activeClass : undefined)}
                         to="/about"
                     >About</NavLink>
                 </li>
                 <li>
                     <NavLink
-                        style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        className={({ isActive }) => (isActive ? activeClass : undefined)}
                         to="/contact"
                     >Contact</NavLink>
                 </li>
--- readme.md
+++ readme.md
@@ -646,19 +646,34 @@ import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
     render() {
-        return (
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
-        )
-    }
+
+      let activeClassName = "active-link";
+
+      return (
+        <ul>
+            <li>
+                <NavLink
+                    style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                    className={({ isActive }) => (isActive ? activeClassName : undefined)}
+                    to="/"
+                >Home</NavLink>
+            </li>
+            <li>
+                <NavLink
+                    style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                    className={({ isActive }) => (isActive ? activeClassName : undefined)}
+                    to="/about"
+                >About</NavLink>
+            </li>
+            <li>
+                <NavLink
+                    style={({ isActive }) => (isActive ? { color: 'tomato' } : undefined)}
+                    className={({ isActive }) => (isActive ? activeClassName : undefined)}
+                    to="/contact"
+                >Contact</NavLink>
+            </li>
+        </ul>
+    )
+  }
 }
 ```
