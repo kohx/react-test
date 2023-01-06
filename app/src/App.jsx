8d0e4837ab6d0d84d78d3b2d3a17adcfada97cc3
okuda <okuda@pass-system.co.jp>
2023-01-06 11:02:47 +0900

    warningを削除

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { MemberList } from './components/MemberList';
 
-export const App = () => {
-  return <div>Hello World!</div>
+// export const App = () => {
+//   return <div>Hello World!</div>
+// }
+
+export class App extends React.Component {
+  render() {
+    return (<h1>Members</h1>);
+  }
 }
---
+++ app/src/components/MemberList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class MemberList extends React.Component {
+    render () {
+        return <h1>Hello, {props.name}</h1>
+    }
+}
