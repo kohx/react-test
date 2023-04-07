d05d4362c6f6e34081ce5df1d6c31ce732239727
okuda <kohei.0728@gmail.com>
2023-04-07 13:45:52 +0900

    add comment

--- app/src/routes/Test12.tsx
+++ app/src/routes/Test12.tsx
@@ -4,19 +4,25 @@ import type { TreeItem } from "../components/Tree/types";
 
 // dummy data received by props
 const codeList = [
-  ["itemA", "1", ""],
+  ["itemA", "1", "6"],
   ["itemB", "2", "5"],
   ["itemC", "3", "1"],
   ["itemD", "4", "2"],
   ["itemE", "5", ""],
-  ["itemF", "6", ""],
+  ["itemF", "6", "9"],
+  ["itemG", "7", "4"],
+  ["itemH", "8", "4"],
+  ["itemI", "9", ""],
 ];
 
 export default () => {
+  // filter no title
   const list0 = codeList.filter((row) => row[0] !== "");
+
+  // set detail
   const list1 = list0.map((row, index): TreeItem => {
     return {
-      id: `${index}: ${row[0]}: ${row[1]}: ${row[2]}`,
+      id: `${index + 1}: ${row[0]} --- [${row[1]}] <${row[2]}>`,
       children: [],
       index,
       title: row[0],
@@ -26,22 +32,32 @@ export default () => {
     };
   });
 
+  // create tree
   const list2 = list1.filter((row) => {
-    const child = row.childKey;
-    if (child !== "") {
-      const parent = list1.find((row) => row.parentKey === child);
+    const childKey = row.childKey;
+    // has parent then 
+    if (childKey !== "") {
+      // get parent by child key
+      const parent = list1.find((row) => row.parentKey === childKey);
+
+      // push child to parent
       parent?.children.push({
         id: row.id,
         children: row.children,
       });
+
+      // not return to list2
       return false;
-    } else {
+    }
+    // has not parent then return list2
+    else {
       return true;
     }
   });
 
-  console.log(list2);
   const [items, setItems] = useState(() => list2);
+  console.log(items);
+  
 
   return (
     <>
